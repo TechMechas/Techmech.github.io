2dd98db09d5fa0617b46f76f5fc52730b399cb1c
kantega <kantega@kantegas-MacBook-Pro.local>
2024-09-17 14:24:21 +0200

    fiks på desktop

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 console.log("hi")
 
-
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', () => {
     const hamburgerMenu = document.querySelector('.hamburger-menu');
     const navLinks = document.querySelector('.nav-links');
 
-    hamburgerMenu.addEventListener('click', function() {
-        navLinks.classList.toggle('active');
+    hamburgerMenu.addEventListener('click', () => {
         hamburgerMenu.classList.toggle('active');
+        navLinks.classList.toggle('active');
     });
 });
+
